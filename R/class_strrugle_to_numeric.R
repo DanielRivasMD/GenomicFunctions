12ac7961b3e21edf47bd07dc12b8f82909a35680
DanielRivasMD <danielrivasmd@gmail.com>
2019-06-06 13:07:40 +0200

    class_strrugle_to_numeric

---
+++ R/class_strrugle_to_numeric.R
@@ -0,0 +1,27 @@
+
+#class_strrugle_to_numeric
+#' @title
+#'
+#'
+#' @description
+#'
+#'
+#' @section Warning:
+#'
+#'
+#' @param f_df
+#'
+#' @param f_class
+#'
+#' @return
+#'
+#' @export
+#' @source
+#'
+#' @examples
+#'
+
+class_strrugle_to_numeric <- function(f_df, f_class){
+  for(i in which(sapply(f_df, class) == f_class)) f_df[[i]] = as.numeric(f_df[[i]])
+  return(f_df)
+}
