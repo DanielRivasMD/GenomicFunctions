509e55cc6cc4b3f2ef5be861ea9e56519f241a61
DanielRivasMD <danielrivasmd@gmail.com>
2019-06-06 13:19:21 +0200

    unique_coor

---
+++ R/unique_coor.R
@@ -0,0 +1,57 @@
+
+#unique_coor
+#' @title
+#'
+#'
+#' @description
+#'
+#'
+#' @section Warning:
+#'
+#'
+#' @param f_query
+#'
+#' @param f_subj
+#'
+#' @param query
+#'
+#' @param subj
+#'
+#' @param f_genomic
+#'
+#' @return
+#'
+#' @export
+#' @source
+#'
+#' @examples
+#'
+
+unique_coor <- function(f_query, f_subj, query, subj, f_genomic=F){
+
+	if(f_genomic==F){
+
+		# require(IRanges)
+		f_query_ranges <- IRanges::IRanges(start=f_query[, 1], end=f_query[, 2])
+		f_subj_ranges <- IRanges::IRanges(start=f_subj[, 1], end=f_subj[, 2])
+	}else{
+
+		# require(GenomicRanges)
+		f_query_ranges <- GenomicRanges::GRanges(seqnames=f_query[, 3], IRanges(start=f_query[, 1], end=f_query[, 2]))
+		f_subj_ranges <- GenomicRanges::GRanges(seqnames=f_subj[, 3], IRanges(start=f_subj[, 1], end=f_subj[, 2]))
+	}
+	f_query_subj <- as.data.frame(IRanges::findOverlaps(f_query_ranges, f_subj_ranges))
+	colnames(f_query_subj) <- c(query, subj)
+	f_query_match <- match(seq_along(f_query_ranges), f_query_subj[, query])
+	unique_pos_query <- f_query[which(is.na(f_query_match)), ]
+	f_subj_match <- match(seq_along(f_subj_ranges), f_query_subj[, subj])
+	unique_pos_subj <- f_subj[which(is.na(f_subj_match)), ]
+	unique_pos_ls <- list(unique_pos_query, unique_pos_subj)
+	names(unique_pos_ls) <- c(query, subj)
+	return(unique_pos_ls)
+
+	# unique_coor takes two data.frames with start (first column) and end (second column) coordinates, and finds unique positions.
+	# f_genomic option F if non-genomic coordinates, otherwise chromosome information must be entered in column 3.
+	# requires GRanges (built-in).
+	# outputs a list of query and subject locations.
+}
