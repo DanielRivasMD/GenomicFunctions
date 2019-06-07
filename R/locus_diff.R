b7d25687a4b6060f13bc64c5d79e3401ccb0d122
DanielRivasMD <danielrivasmd@gmail.com>
2019-06-07 13:04:05 +0200

    Format locus_diff

--- R/locus_diff.R
+++ R/locus_diff.R
@@ -1,29 +1,25 @@
 
-#locus_diff
-#' @title
-#'
+#' @title locus_diff
 #'
 #' @description
+#' \emph{locus_diff}
 #'
+#' @param f_x f_x
+#' @param f_y f_y
+#' @param f_data f_data
 #'
-#' @section Warning:
+#' @return return
 #'
-#'
-#' @param f_x
-#'
-#' @param f_y
-#'
-#' @param f_data
-#'
-#' @return
-#'
+#' @seealso \code{\link{peak_diff}}
+#' @seealso \code{\link{h_clust}}
 #' @export
-#' @source
-#'
-#' @examples
-#'
 
-locus_diff <- function(f_x, f_y, f_data){
+locus_diff <- function(
+
+	f_x,
+	f_y,
+	f_data
+) {
 
 	out_val <- sum(abs(f_data[, f_x] - f_data[, f_y]))
 	return(out_val)
