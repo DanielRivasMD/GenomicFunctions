b51450b1e9859727ffac6e75c0440ee01087cbb6
DanielRivasMD <danielrivasmd@gmail.com>
2019-06-07 13:03:18 +0200

    Format h_clust

--- R/h_clust.R
+++ R/h_clust.R
@@ -1,35 +1,30 @@
 
-#h_clustering
-#' @title
-#'
+#' @title Hierarchical Cluster Tool
 #'
 #' @description
+#' \emph{h_clust} provides hierarchical clustering for group(s) of populations, i. e. it calculates through \emph{peak_diff} and constructs a matrix of the absolute differences among populations
+#' It recives a data.frame of populations as \emph{f_dataset} where a vector of column names to be compare must be specified as \emph{f_pop}
 #'
+#' @param f_dataset matrix to values
+#' @param f_pop population names
 #'
-#' @section Warning:
+#' @return return
 #'
-#'
-#' @param f_dataset
-#'
-#' @param f_pop
-#'
-#' @return
-#'
+#' @seealso \code{\link{locus_diff}}
+#' @seealso \code{\link{peak_diff}}
 #' @export
-#' @source
-#'
-#' @examples
-#'
 
-h_clust <- function(f_dataset, f_pop){
+h_clust <- function(
+
+	f_dataset,
+	f_pop
+) {
 
 	f_n_pop <- length(f_pop)
-	hcluster <- matrix(data=mapply(peak_diff, f_x=rep(1:f_n_pop, times=f_n_pop), f_y=rep(1:f_n_pop, each=f_n_pop), MoreArgs=list(f_data=f_dataset[, f_pop])), nrow=f_n_pop)
+	hcluster <- matrix(data = mapply(peak_diff, f_x = rep(1:f_n_pop, times = f_n_pop), f_y = rep(1:f_n_pop, each = f_n_pop), MoreArgs = list(f_data = f_dataset[, f_pop])), nrow = f_n_pop)
 	diag(hcluster) <- NA
 	colnames(hcluster) <- f_pop
 	rownames(hcluster) <- f_pop
-	return(hcluster)
 
-	# h_clust provides h-clustering for group(s) of populations, i. e. it calculates through "peak_diff" and constructs a matrix of the absolute differences among populations.
-	# It recives a data.frame of populations (f_dataset) where a vector of column names to be compare must be specified (f_pop).
+	return(hcluster)
 }
