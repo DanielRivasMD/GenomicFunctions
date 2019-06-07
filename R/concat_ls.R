785cea9236428e358c07d2787c9bb6d4ba1bc732
DanielRivasMD <danielrivasmd@gmail.com>
2019-06-07 12:56:32 +0200

    Format concat_ls

--- R/concat_ls.R
+++ R/concat_ls.R
@@ -1,66 +1,60 @@
 
-#concat_ls
-#' @title
-#'
+#' @title Concatenate List
 #'
 #' @description
+#' \emph{concat_ls} inputs a list of elements and concatenates them
 #'
+#' @param f_data List of elements
 #'
-#' @section Warning:
-#'
-#'
-#' @param f_data
-#'
-#' @return
+#' @return Concataned element of the same class as input
 #'
 #' @export
-#' @source
-#'
-#' @examples
-#'
 
-concat_ls <- function(f_data){
+concat_ls <- function(
 
-	if("data.frame" %in% class(f_data[[1]]) | "matrix" %in% class(f_data[[1]])){
+	f_data
+) {
 
-		for(f_looping in 1:length(f_data)){
-
-			if(f_looping==1){
-
+	if ( "data.frame" %in% class(f_data[[1]]) | "matrix" %in% class(f_data[[1]]) ) {
+		#
+		for ( f_looping in 1:length(f_data) ) {
+			#
+			if ( f_looping == 1 ) {
+				#
 				f_array <- f_data[[f_looping]]
-			}else{
-
-				if(dim(f_data[[f_looping]])[1]>0){
-
+			} else {
+				#
+				if ( dim(f_data[[f_looping]])[1] > 0 ) {
+					#
 					f_array[(1:dim(f_data[[f_looping]])[1])+dim(f_array)[1], ] <- f_data[[f_looping]]
 				}
 			}
 		}
-	}else{
-
-		for(f_looping in 1:length(f_data)){
-
-			if(f_looping==1){
-
-				if(class(f_data[[f_looping]])=="table"){
-
+	} else {
+		#
+		for ( f_looping in 1:length(f_data) ) {
+			#
+			if ( f_looping == 1 ) {
+				#
+				if ( class(f_data[[f_looping]]) == "table" ) {
+					#
 					f_names <- names(f_data[[f_looping]])
 				}
 				f_array <- f_data[[f_looping]]
-
-			}else{
-
+				#
+			} else {
+				#
 				f_array[seq_along(f_data[[f_looping]])+length(f_array)] <- f_data[[f_looping]]
-
-				if(class(f_data[[f_looping]])=="table"){
-
+				#
+				if ( class(f_data[[f_looping]]) == "table" ) {
+					#
 					f_names[seq_along(f_data[[f_looping]])+length(f_names)] <- names(f_data[[f_looping]])
 				}
 			}
 		}
-
-		if(class(f_data[[f_looping]])=="table"){
-
+		#
+		if ( class(f_data[[f_looping]]) == "table" ) {
+			#
 			names(f_array) <- f_names
 		}
 	}
