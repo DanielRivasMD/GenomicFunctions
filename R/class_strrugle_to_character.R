13d36c98a72a2e469efc63a05e73f039451c25eb
DanielRivasMD <danielrivasmd@gmail.com>
2019-06-07 12:55:54 +0200

    Format class_strrugle

--- R/class_strrugle_to_character.R
+++ R/class_strrugle_to_character.R
@@ -1,27 +1,23 @@
 
-#class_strrugle_to_character
-#' @title
-#'
+#' @title class_strrugle_to_character
 #'
 #' @description
+#' \emph{class_strrugle_to_character}
 #'
+#' @param f_df f_df
+#' @param f_class f_class
 #'
-#' @section Warning:
+#' @return return
 #'
-#'
-#' @param f_df
-#'
-#' @param f_class
-#'
-#' @return
-#'
+#' @seealso \code{\link{class_strrugle_to_numeric}}
 #' @export
-#' @source
-#'
-#' @examples
-#'
 
-class_strrugle_to_character <- function(f_df, f_class){
-  for(i in which(sapply(f_df, class) == f_class)) f_df[[i]] = as.character(f_df[[i]])
-  return(f_df)
+class_strrugle_to_character <- function(
+
+	f_df,
+	f_class
+) {
+
+	for(i in which(sapply(f_df, class) == f_class)) f_df[[i]] = as.character(f_df[[i]])
+	return(f_df)
 }
--- R/class_strrugle_to_numeric.R
+++ R/class_strrugle_to_numeric.R
@@ -1,27 +1,22 @@
 
-#class_strrugle_to_numeric
-#' @title
-#'
+#' @title class_strrugle_to_numeric
 #'
 #' @description
+#' \emph{class_strrugle_to_numeric}
 #'
+#' @inheritParams class_strrugle_to_character
 #'
-#' @section Warning:
+#' @return return
 #'
-#'
-#' @param f_df
-#'
-#' @param f_class
-#'
-#' @return
-#'
+#' @seealso \code{\link{class_strrugle_to_character}}
 #' @export
-#' @source
-#'
-#' @examples
-#'
 
-class_strrugle_to_numeric <- function(f_df, f_class){
-  for(i in which(sapply(f_df, class) == f_class)) f_df[[i]] = as.numeric(f_df[[i]])
-  return(f_df)
+class_strrugle_to_numeric <- function(
+
+	f_df,
+	f_class
+) {
+
+	for(i in which(sapply(f_df, class) == f_class)) f_df[[i]] = as.numeric(f_df[[i]])
+	return(f_df)
 }
